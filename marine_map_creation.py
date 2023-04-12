3b8548aabe9dd199aec4cfb22ae9198984c18018
katiek23 <129174170+katiek23@users.noreply.github.com>
2023-04-12 14:17:37 +0100

    Creating marine_map_creation python file

---
+++ marine_map_creation.py
@@ -0,0 +1,11 @@
+# First lets import the functions and modules we need to create the map
+
+%matplotlib inline
+
+import os
+import geopandas as gpd
+import matplotlib.pyplot as plt
+from cartopy.feature import ShapelyFeature
+import cartopy.crs as ccrs
+import matplotlib.patches as mpatches
+import matplotlib.lines as mlines
