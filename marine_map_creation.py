4d6c66fd28e6d8db70717c9b37866df80e118576
katiek23 <129174170+katiek23@users.noreply.github.com>
2023-04-20 12:03:47 +0100

    Adding data files

--- marine_map_creation.py
+++ marine_map_creation.py
@@ -1,11 +1,18 @@
+# Creation of map
+
 # First lets import the functions and modules we need to create the map
 
 %matplotlib inline
 
 import os
+import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
 from cartopy.feature import ShapelyFeature
 import cartopy.crs as ccrs
 import matplotlib.patches as mpatches
 import matplotlib.lines as mlines
+from geopandas import GeoDataFrame
+from shapely.geometry import Point
+
+#Loading the data
